615b86061240e8ab732af592693d660c42615870
Ezer Silva <ezersilva@gmail.com>
2022-01-20 16:16:41 -0300

    now there is an option to cluster features in the relationship plot

--- src/eda.py
+++ src/eda.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 import ipywidgets as widgets
 from IPython.display import display
+import scipy
+import scipy.cluster.hierarchy as sch
+
 
 #%% df for testing purposes
 dataset = load_iris()
@@ -15,6 +18,37 @@ df = pd.DataFrame(
     )
 y = dataset.target
 df['y'] = y
+
+#%% Utility functions
+
+def cluster_corr(corr_array, inplace=False):
+    """
+    Rearranges the correlation matrix, corr_array, so that groups of highly 
+    correlated variables are next to eachother 
+    
+    Parameters
+    ----------
+    corr_array : pandas.DataFrame or numpy.ndarray
+        a NxN correlation matrix 
+        
+    Returns
+    -------
+    pandas.DataFrame or numpy.ndarray
+        a NxN correlation matrix with the columns and rows rearranged
+    """
+    pairwise_distances = sch.distance.pdist(corr_array)
+    linkage = sch.linkage(pairwise_distances, method='complete')
+    cluster_distance_threshold = pairwise_distances.max()/2
+    idx_to_cluster_array = sch.fcluster(linkage, cluster_distance_threshold, 
+                                        criterion='distance')
+    idx = np.argsort(idx_to_cluster_array)
+    
+    if not inplace:
+        corr_array = corr_array.copy()
+    
+    if isinstance(corr_array, pd.DataFrame):
+        return corr_array.iloc[idx, :].T.iloc[idx, :]
+    return corr_array[idx, :][:, idx]
 
 # %%
 def _show_interactive_plot(
@@ -147,6 +181,7 @@ def show_scatterplot(
 
 def show_feature_relationship(
     df,
+    cluster_features=True,
     features=None,
     relation_func=lambda df: df.corr(),
     **kwargs,
@@ -166,6 +201,8 @@ def show_feature_relationship(
     def _plot(selected_features):
         plt.figure(figsize=(10,8))
         df_rel = relation_func(df[list(selected_features)])
+        if cluster_features:
+            df_rel = cluster_corr(df_rel)
         sns.heatmap(df_rel, **kwargs)
         plt.show()
     
@@ -173,4 +210,30 @@ def show_feature_relationship(
 
 
 # %%
+def show_sorted_stat(
+    df,
+    features=None,
+    stat_func=lambda df: df.mean(),
+    **kwargs,
+    ):
+    if features is None:
+        features = list(df.columns)
+    if 'style' not in kwargs:
+        kwargs['style'] = '.'
+
+    select_features = widgets.SelectMultiple(
+        options=list(features),
+        value=features[:min(3, len(features))],
+        description='Features:',
+        disabled=False,
+    )
+    
+    def _plot(selected_features):
+        plt.figure(figsize=(10,8))
+        df_stats = stat_func(df[list(selected_features)])
+        df_stats = df_stats.sort_values().plot(**kwargs)
+        plt.show()
+    
+    _show_interactive_plot(df, features, _plot, {'selected_features': select_features})
+
 # %%
